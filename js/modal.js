96044df210c67d598b3e59c87762d5396507ca7e
Niyaz Garipov <Niayz_Indicus@mail.ru>
2018-08-11 15:31:19 +0300

    Корректирует файлы

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,32 @@
 'use strict';
 
-var write = document.querySelector(".button__modal");
-var modal = document.querySelector(".feedback");
-var close = document.querySelector(".feedback__close");
-var focus = modal.querySelector("[name=user-name]");
+(function () {
+  var ESC_CODE = 27;
 
-// открытие модального окна
-write.addEventListener("click", function(e) {
-  e.preventDefault();
-  modal.classList.add("feedback__show");
-  // Устанавливаем фокус в поле
-  focus.focus();
-});
+  var write = document.querySelector(".button__modal");
+  var modal = document.querySelector(".feedback");
+  var close = document.querySelector(".feedback__close");
+  var focus = modal.querySelector("[name=user-name]");
 
-// закрытие модального окна
-close.addEventListener("click", function(e) {
-  e.preventDefault();
-  modal.classList.remove("feedback__show");
-});
+  var escPressHandler = function (evt) {
+    if (evt.keyCode === ESC_CODE) {
+      modal.classList.remove("feedback__show");
+    }
+  }
+
+  // открытие модального окна
+  write.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modal.classList.add("feedback__show");
+    // Устанавливаем фокус в поле
+    focus.focus();
+
+    window.addEventListener('keydown', escPressHandler);
+  });
+
+  // закрытие модального окна
+  close.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modal.classList.remove("feedback__show");
+  });
+})();
