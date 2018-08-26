e822a31ceea76ef0645f3f02dedef0c4a1068330
Niyaz Garipov <Niayz_Indicus@mail.ru>
2018-08-26 09:37:02 +0300

    Подключает js файлы

---
+++ js/map.js
@@ -0,0 +1,16 @@
+'use strict';
+
+ function initMap() {
+  var uluru = {
+    lat: 59.938861,
+    lng: 30.323154
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+};
